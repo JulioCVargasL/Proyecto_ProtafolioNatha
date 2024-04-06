460d5befc820380ffd4ca7c47b0fac2c77dcd643
Andres Bayona <andresbayona408@gmail.com>
2024-04-05 20:38:23 -0500

    En clase 5/4/2024

--- formulario/models.py
+++ formulario/models.py
@@ -13,7 +13,7 @@ class Pagos(models.Model):
 
   pay_ID        = models.AutoField(primary_key= True)
   cotizacion    = models.IntegerField()
-  pay_Comment   = models.TextField()
+  pay_comment   = models.TextField()
   pay_status    = models.CharField(choices=STATUS_OPTIONS)
   pay_date      = models.DateTimeField()
 
@@ -27,28 +27,30 @@ class Sesion(models.Model):
     ("Evento empresarial",  "Opcion 6")
   ]
 
-  ses_ID        = models.AutoField(Primary_key= True)
-  ses_date      = models.DateTimeField()
-  ses_type      = models.CharField(choices=EVENT_TYPE)
-  ses_comment   = models.TextField()
- 
-  pagos         = models.ForeignKey(Pagos, on_delete=models.PROTECT)  
+  ses_ID          = models.AutoField(Primary_key= True)
+  ses_date        = models.DateTimeField()
+  ses_type        = models.CharField(choices=EVENT_TYPE)
+  ses_comment     = models.TextField()
+  # Foreing Key(s)
+  pagos           = models.ForeignKey(Pagos, on_delete=models.PROTECT)  
 
 class Usuario(models.Model):
 
-  user_ID       = models.AutoField(primary_key= True)
-  user_doc      = models.IntegerField()
-  user_name     = models.CharField(max_length=40, blank= False, null= False)
-  user_email    = models.EmailField()
-  user_pasword  = models.CharField(max_length=10, blank= False, null= False)
+  user_ID         = models.AutoField(primary_key= True)
+  user_doc        = models.IntegerField()
+  user_name       = models.CharField(max_length=40, blank= False, null= False)
+  user_email      = models.EmailField()
+  user_password   = models.CharField(max_length=10, blank= False, null= False)
+  # Foreing Key(s)
+  sesion          = models.ForeignKey(Pagos, on_delete=models.PROTECT)          
 
 class Administrador(models.Model):
 
-  admin_ID      = models.AutoField(primary_key= True)
-  admin_doc     = models.IntegerField()
-  admin_name    = models.CharField(max_length=40, blank= False, null= False)
-  admin_email   = models.EmailField()
-  admin_pasword = models.CharField(max_length=10, blank= False, null= False)
-
+  admin_ID        = models.AutoField(primary_key= True)
+  admin_doc       = models.IntegerField()
+  admin_name      = models.CharField(max_length=40, blank= False, null= False)
+  admin_email     = models.EmailField()
+  admin_password  = models.CharField(max_length=10, blank= False, null= False)
+  # Foreing Key(s)
   usuario       = models.ForeignKey(Usuario, on_delete=models.PROTECT)
 
