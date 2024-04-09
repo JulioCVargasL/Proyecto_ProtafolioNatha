a89185543f7f25ad6e5b50f7cc37d67f4b2c6232
JulioCVargasL <2.0.jcvl92@gmail.com>
2024-04-09 01:25:22 -0500

    Formulario de pagos

--- formulario/urls.py
+++ formulario/urls.py
@@ -3,4 +3,5 @@ from .              import views
 
 urlpatterns = [
     path('registrar/', views.registrar, name="registrar"),
+    path('registrar_pago/', views.registrar_pago, name="registrar_pago"),
 ]
--- formulario/views.py
+++ formulario/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.http      import HttpResponse
+from .models          import Pagos
 
 # Create your views here.
 
 def registrar(request):
   return render(request, 'registrar.html', {})
+
+#  metodo POST  y empleando un formulario
+def registrar_pago(request):
+  cotizacion   = request.GET['cotizacion']
+  pay_comment  = request.GET['pay_comment']
+  pay_status   = request.GET['pay_status']
+  pay_date     = request.GET['pay_date']
+
+  Pagos.objects.create(cotizacion=cotizacion, pay_comment=pay_comment, pay_status=pay_status, pay_date=pay_date)
+  return  HttpResponse("Se registro el pago con exito")
