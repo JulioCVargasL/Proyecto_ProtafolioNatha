53a8370ca06bf11db96dfb69624aad3717317834
JulioCVargasL <2.0.jcvl92@gmail.com>
2024-04-10 02:15:25 -0500

    EN casa

--- formulario/urls.py
+++ formulario/urls.py
@@ -4,4 +4,5 @@ from .              import views
 urlpatterns = [
     path('registrar/', views.registrar, name="registrar"),
     path('registrar_pago/', views.registrar_pago, name="registrar_pago"),
+    path('listar_pagos/', views.listar, name="listar_pagos"),
 ]
--- formulario/views.py
+++ formulario/views.py
@@ -5,14 +5,21 @@ from .models          import Pagos
 # Create your views here.
 
 def registrar(request):
-  return render(request, 'registrar.html', {})
+  return render(request, 'registrar_pago.html', {})
 
 #  metodo POST  y empleando un formulario
 def registrar_pago(request):
-  cotizacion   = request.GET['cotizacion']
-  pay_comment  = request.GET['pay_comment']
-  pay_status   = request.GET['pay_status']
-  pay_date     = request.GET['pay_date']
+  cotizacion   = request.POST['cotizacion']
+  pay_comment  = request.POST['pay_comment']
+  pay_status   = request.POST['pay_status']
+  pay_date     = request.POST['pay_date']
 
   Pagos.objects.create(cotizacion=cotizacion, pay_comment=pay_comment, pay_status=pay_status, pay_date=pay_date)
   return  HttpResponse("Se registro el pago con exito")
+
+def listar(request):
+  pagos = Pagos.objects.all()
+
+  return render(request, 'listar_pagos.html', {
+    'pagos': pagos
+  })
