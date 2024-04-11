189ec9f8a513fffed8dd29646515e9d9e6f4dafa
JulioCVargasL <2.0.jcvl92@gmail.com>
2024-04-10 23:45:11 -0500

    Creando el Delete para tabla pagos

--- formulario/urls.py
+++ formulario/urls.py
@@ -2,7 +2,10 @@ from django.urls    import path
 from .              import views
 
 urlpatterns = [
-    path('registrar/', views.registrar, name="registrar"),
-    path('registrar_pago/', views.registrar_pago, name="registrar_pago"),
-    path('listar_pagos/', views.listar, name="listar_pagos"),
+    path('registrar/',         views.registrar,        name="registrar"),
+    path('registrar_pago/',    views.registrar_pago,   name="registrar_pago"),
+    path('listar_pagos/',      views.listar,           name="listar_pagos"),
+    path('show_editar_pago/',  views.show_editar_pago, name="show_editar_pago"),
+    path('editar_pago/',       views.editar_pago,      name="editar_pago"),
+    path('delete_pago/<int:id>',views.delete_pago, name="eliminar_pago"), 
 ]
--- formulario/views.py
+++ formulario/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http      import HttpResponse
 from .models          import Pagos
 
@@ -23,3 +23,30 @@ def listar(request):
   return render(request, 'listar_pagos.html', {
     'pagos': pagos
   })
+
+def show_editar_pago(request):
+  return render(request, 'editar_pagos.html', {})
+
+# en esta secion editamos los registros de pago 
+def editar_pago(request):
+  id_pago      = request.POST['id_pago']
+  cotizacion   = request.POST['cotizacion']
+  pay_comment  = request.POST['pay_comment']
+  pay_status   = request.POST['pay_status']
+  pay_date     = request.POST['pay_date']
+
+  editar       = Pagos.objects.get(id=id_pago)
+
+  editar.cotizacion   = cotizacion
+  editar.pay_comment  = pay_comment
+  editar.pay_status   = pay_status
+  editar.pay_date     = pay_date
+  editar.save()
+
+  return HttpResponse("Se ha editado con exito por formulario")
+
+def delete_pago(request, id):
+  pago = Pagos.objects.get(id=id)
+  pago.delete()
+
+  return redirect('listar_pagos')
